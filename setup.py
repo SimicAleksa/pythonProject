7415147f432cf1ca750414e5b1d3b790582b80cc
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2023-12-24 11:51:20 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='1.1',
+    packages=['if_dsl_gui_ai'],
+    url='',
+    license='',
+    author='Aleksa',
+    author_email='',
+    description=''
+)
